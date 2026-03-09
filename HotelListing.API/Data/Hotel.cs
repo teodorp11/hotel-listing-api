7a5c293ccd2f3036ec47f3d4cf4ea221c25c4d34
teodorp11 <teodor.posea@gmail.com>
2026-03-09 20:35:47 +0200

    feat: create Data folder and add Hotel model

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set;  }
+        public double Rating { set; get; }
+    }
+}
