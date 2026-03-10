f0dae87e8bb3b7922f068ff635d51f0821ae0071
teodorp11 <teodor.posea@gmail.com>
2026-03-10 10:35:05 +0200

    feat: add Country model and link to Hotel entity
    - Created Country.cs with basic properties and a collection of Hotels
    - Updated Hotel.cs to include CountryId foreign key and navigation property
    - Established a one-to-many relationship between Countries and Hotels.

--- HotelListing.API/Data/Hotel.cs
+++ HotelListing.API/Data/Hotel.cs
@@ -6,5 +6,10 @@
         public string Name { get; set; }
         public string Address { get; set;  }
         public double Rating { set; get; }
+
+        // Foreign Key for Country
+        public int CountryId { get; set; }
+        // Reference to the parent Country entity (Navigation Property)
+        public Country? Country { get; set; }
     }
 }
