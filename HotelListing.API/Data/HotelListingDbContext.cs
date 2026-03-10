e34916c3a54cbc83f0bfd2bba76ad71be65272a6
teodorp11 <teodor.posea@gmail.com>
2026-03-10 10:49:07 +0200

    feat: create HotelListingDbContext and define DbSets
    - Implemented HotelListingDbContext inheriting from DbContext
    - Added DbSet properties for Hotels and Countries to enable database mapping
    - Configured constructor to support Dependency Injection via DbContextOptions.

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
